d23dcd9e5fb26cc5f4a33162681119c149b49d63
Jorge Garcia <jagbolanos@Jorges-MacBook-Pro-2.local>
2020-02-14 22:22:49 -0700

    PinataCall works

--- app/src/main/java/com/notarize/app/MainActivity.kt
+++ app/src/main/java/com/notarize/app/MainActivity.kt
@@ -2,7 +2,9 @@ package com.notarize.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
 import android.util.Log
+import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -23,13 +25,25 @@ class MainActivity : AppCompatActivity() {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
             var pinata = retrofit.create(PinataService::class.java)
-            pinata.pinList().enqueue(object : Callback<String> {
-                override fun onFailure(call: Call<String>, t: Throwable) {
-                    Log.d("TEST", "Something failed")
+            var call = pinata.pinList()
+            //StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
+            //var response = call.execute()
+            //Log.d("TEST", response.message())
+            pinata.pinList().enqueue(object : Callback<JsonObject> {
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.d("TEST", t.message)
                 }
 
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    Log.d("TEST", "Something worked")
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+
+                    if (response.code() == 200) {
+                        Log.d("TEST", "Something worked")
+                        val body = response.body()!!
+                        Log.d("TEST", body.toString())
+                    } else {
+                        Log.d("TEST", "Something Kind Of worked")
+                        Log.d("TEST", "CODE " + response.code())
+                    }
                 }
 
             })
--- app/src/main/java/com/notarize/app/PinataService.java
+++ app/src/main/java/com/notarize/app/PinataService.java
@@ -1,11 +1,15 @@
 package com.notarize.app;
 
+import com.google.gson.JsonObject;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 
 public interface PinataService {
 
     @GET("data/pinList?status=pinned")
-    Call<String> pinList();
+    Call<JsonObject> pinList();
 }
