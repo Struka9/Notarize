ef20cafed910de349639e0e118b2420f98750af7
Jorge Garcia <jagbolanos@Jorges-MacBook-Pro-2.local>
2020-02-15 01:39:16 -0700

    multipart upload works from String

--- app/src/main/java/com/notarize/app/MainActivity.kt
+++ app/src/main/java/com/notarize/app/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.notarize.app
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.FileUtils
 import android.os.StrictMode
 import android.util.Log
 import com.google.gson.JsonObject
@@ -11,6 +13,10 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.MediaType
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,11 +31,33 @@ class MainActivity : AppCompatActivity() {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
             var pinata = retrofit.create(PinataService::class.java)
-            var call = pinata.pinList()
-            //StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
-            //var response = call.execute()
-            //Log.d("TEST", response.message())
-            pinata.pinList().enqueue(object : Callback<JsonObject> {
+
+
+            /*pinata.pinList().enqueue(object : Callback<JsonObject> {
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.d("TEST", t.message)
+                }
+
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+
+                    if (response.code() == 200) {
+                        Log.d("TEST", "Something worked")
+                        val body = response.body()!!
+                        Log.d("TEST", body.toString())
+                    } else {
+                        Log.d("TEST", "Something Kind Of worked")
+                        Log.d("TEST", "CODE " + response.code())
+                    }
+                }
+
+            })*/
+
+            // Create a request body with file and image media type
+            val fileReqBody = RequestBody.create(MediaType.parse("text/plain"), "This is the file content")
+            // Create MultipartBody.Part using file request-body,file name and part name
+            val part = MultipartBody.Part.createFormData("file", "file3.txt", fileReqBody)
+
+            pinata.pinFileToIPFS(part).enqueue(object : Callback<JsonObject> {
                 override fun onFailure(call: Call<JsonObject>, t: Throwable) {
                     Log.d("TEST", t.message)
                 }
@@ -47,9 +75,6 @@ class MainActivity : AppCompatActivity() {
                 }
 
             })
-
-
-
         }
     }
 }
--- app/src/main/java/com/notarize/app/PinataService.java
+++ app/src/main/java/com/notarize/app/PinataService.java
@@ -2,14 +2,20 @@ package com.notarize.app;
 
 import com.google.gson.JsonObject;
 
+import okhttp3.MultipartBody;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface PinataService {
 
     @GET("data/pinList?status=pinned")
     Call<JsonObject> pinList();
+
+    @Multipart
+    @POST("pinning/pinFileToIPFS")
+    Call<JsonObject> pinFileToIPFS(@Part MultipartBody.Part file);
 }
